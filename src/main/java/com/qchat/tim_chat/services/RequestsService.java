dac4f8bf702eb3bd025f1bb5b8c7334756bbb6ff
LogicCat <140593301+MaisyCat@users.noreply.github.com>
2026-03-12 21:33:18 +0800

    Delete src directory

--- src/main/java/com/qchat/tim_chat/services/RequestsService.java
+++
@@ -1,9 +0,0 @@
-package com.qchat.tim_chat.services;
-
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-@Service
-public interface RequestsService {
-    List<Long> get_sender_uid(Long user_uid);
-}
